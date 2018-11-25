5224c00e1eb7e79eded276e57a4a0f669bf07172
Alex Cardelle <acardelle@icloud.com>
2018-11-24 22:33:57 -0500

    Completed 4 of 4 Assignments

--- src/exercises/SearchAlice.java
+++ src/exercises/SearchAlice.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class SearchAlice {
     public static void main(String[] args) {
-        String[] passage = {"Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?"};
+        String passage = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?";
+        String[] words = passage.split("\\s+");
         String searchTerm;
         Scanner askTerm;
         boolean found = false;
@@ -13,7 +14,7 @@ public class SearchAlice {
         System.out.println("What is your search term?");
         searchTerm = askTerm.next();
 
-        for (String element:passage) {
+        for (String element:words) {
             if ( element.equals( searchTerm )) {
                 found = true;
             }
