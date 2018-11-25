664e8579926e38ed12fde53cfe3dca4d53a521b9
Alex Cardelle <acardelle@icloud.com>
2018-11-25 14:52:27 -0500

    Completed Area Studio

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -1,7 +1,23 @@
 package org.launchcode.java.studios;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        double r;
+        double area;
+        Scanner askR;
 
-    public static void main(String[] args)
 
+        askR = new Scanner(System.in);
+        System.out.println("Enter your circle's radius:");
+        r = askR.nextDouble();
+
+        area = pi * r * r;
+        System.out.println("Your circle's area is: " + area);
+
+
+
+    }
 }
